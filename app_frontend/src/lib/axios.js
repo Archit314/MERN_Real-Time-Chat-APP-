4af07a3c06e30b5bfbdc2b11db4bafe0effa1c3e
Archit Kumar <architkumar314@gmail.com>
2025-02-25 06:35:52 +0530

    Frontend - Added .env file

--- app_frontend/src/lib/axios.js
+++ app_frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_APP_API_BASE_URL,
     withCredentials: true
 })
